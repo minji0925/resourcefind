0c642112753639e24c081c1acd4ceffbf1557462
1006mjkim <1006mjkim@gmail.com>
2023-06-09 20:08:09 +0900

    updated user authentication and recommendation

--- spotipy-music-project/spotipy_auth2.py
+++ spotipy-music-project/spotipy_auth2.py
@@ -2,11 +2,12 @@ from flask import Flask, request
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+#sp setup
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
         client_id='7c0d4573b37f464ea22f821012fd1ead',
         client_secret='81cf0e62bda04efbb6ffbb716e7c7e3d',
         redirect_uri='http://localhost:8000/callback',
-        scope='user-library-read'
+        scope='playlist-modify-public'
     ))
 
 app = Flask(__name__)
@@ -15,6 +16,7 @@ app = Flask(__name__)
 def home():
     return 'Hello, World!'
 
+#user authentication process when accessing a /callback link
 @app.route('/callback')
 def callback():
     auth_code = request.args.get('code')    
@@ -24,16 +26,20 @@ def callback():
 if __name__ == '__main__':
     app.run(port=8000)
 
+#print genre list
 print("Genre options: "+ str(sp.recommendation_genre_seeds()))
 genre = input("Enter genre: ")
 
-recommendations = sp.recommendations(seed_genres=[genre], target_acousticness = 1)
-#for i in recommendations["tracks"][0]["artists"]:
-#    print(i["name"])
-#print(recommendations["tracks"][0])
+#recommendations based on desired genre & target acousticness (ill add more options later)
+recommendations = sp.recommendations(seed_genres=[genre], target_acousticness = 0.9)
 
+#get current user and create a new playlist
+user_info = sp.current_user()
+user_id = user_info['id']
+playlist = sp.user_playlist_create(user_id, 'Recommendations in '+genre, public=True, collaborative=False, description='Spotipy music recommendations')
+playlist_id = playlist['id']
+
+#print the recommended tracks and add each of them to the created playlist
 for i in recommendations["tracks"]:
     print(i["name"] + ": " + i["artists"][0]["name"])
-#print(recommendations["tracks"][0]["name"])
-
-#print(sp.current_user_playing_track.target_popularity)
+    sp.playlist_add_items(playlist_id, [i["uri"]])
